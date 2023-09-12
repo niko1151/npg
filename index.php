bc7bac050d0505f12325901f7827df3a5b862fa1
Nikolai <nsflodin@gmail.com>
2023-09-12 13:15:43 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+use tec\npg;
+use tec\npg\Controllers\UserController;
+
+
+require __DIR__ . '/vendor/autoload.php';
+
+session_start();
+
+function isUserLoggedIn() {
+  return isset($_SESSION['username']);
+}
+
+// Load environment variables from .env file in the project root directory.
+// $dotenv = Dotenv\Dotenv::create(__DIR__);
+// $dotenv->load();
+
+Flight::route('/', function(){
+  Flight::render('frontpage', array('body'), 'body_content');
+  Flight::render('layout', array('title' => 'Forside - NPG'));
+ });
+
+ Flight::route('/about', function(){
+   Flight::render('about', array('body'), 'body_content');
+   Flight::render('layout', array('title' => 'Om os - NPG'));
+});
+
+Flight::route('/login', function(){
+  Flight::render('login', array('body'), 'body_content');
+  Flight::render('layout', array('title' => 'Login - NPG'));
+});
+
+Flight::route('/logout', function () {
+  // Handle logout logic (destroy session, etc.)
+  session_start();
+  session_destroy();
+  // Redirect to the homepage or login page
+  Flight::redirect('/');
+});
+
+Flight::route('/category', function(){
+  Flight::render('category', array('body'), 'body_content');
+  Flight::render('layout', array('title' => 'Kategori - NPG'));
+});
+
+Flight::start();
